7fb7f8641b1510556befa958e80316a34afae073
Poro-hentai <shadowsama128@gmail.com>
2025-04-17 19:51:56 +0530

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -148,7 +148,7 @@ async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
 # --- MAIN ---
 if __name__ == "__main__":
     import asyncio
-TOKEN = 7363840731:AAFGAop4T9tLSSajj2365wEzNbeGnrW845s")  # Replace with your bot token
+TOKEN = 7363840731:AAFaheLlW0QCb3-N4-s2xyPXnnLUboATPYs")  # Replace with your bot token
 
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
