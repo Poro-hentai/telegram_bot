3429e8a597d6831476c4cdd77a1ce094ae3e43c0
Poro-hentai <shadowsama128@gmail.com>
2025-06-20 00:10:59 +0530

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -176,7 +176,8 @@ bot_app.add_handler(CommandHandler("autorename", autorename))
 bot_app.add_handler(CommandHandler("thumburl", thumburl))
 bot_app.add_handler(CommandHandler("broadcast", broadcast))
 bot_app.add_handler(CallbackQueryHandler(callback_handler))
-bot_app.add_handler(MessageHandler(filters.Document.ALL | filters.Video.ALL | filters.PHOTO | filters.Audio.ALL, handle_file))
+bot_app.add_handler(MessageHandler(filters.Document.ALL | filters.VIDEO | filters.PHOTO | filters.AUDIO, handle_file))
+
 bot_app.add_handler(MessageHandler(filters.COMMAND, unknown))
 
 if __name__ == '__main__':
